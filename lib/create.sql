3322962926602b654cf4b6a762a881af339bdee2
jwillie-26 <105637783+jwillie-26@users.noreply.github.com>
2023-02-15 08:31:14 +0300

    added solution

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears(
+id INTEGER PRIMARY KEY,
+name TEXT,
+age REAL,
+sex TEXT,
+color TEXT,
+temperament TEXT,
+alive BOOLEAN
+);
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 1, 'M', 'Brown', 'Sweet', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 2, 'M', 'Black', 'Prickly', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 3, 'F', 'Brown', 'Sweet', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 4, 'M', 'Aggressive', 'Sweet', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 5, 'F', 'Black', 'Compliant', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 6, 'M', 'Grey', 'Loud', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 7, 'F', 'White', 'Sweet', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (Null, 8, 'M', 'Brown', 'Mild tempered', 1);
